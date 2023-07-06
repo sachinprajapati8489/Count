dedc1d243d5c1230f7d82de58bb54a93ee27cbb8
sachinparjapati011 <sachinparjapati011@gmail.com>
2023-07-06 09:53:12 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
+import React, { useRef, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [value, setValue] = useState(0);
+  const step = useRef(0);
+
+  const setStep = (value) => {
+    step.current = value;
+  };
+
+  const increment = () => {
+    setValue((state) => state + step.current);
+  };
+
+  const decrement = () => {
+    setValue((state) => state - step.current);
+  };
+
+  const reset = () => {
+    setValue(0);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container text-center my-5">
+        <h1>{value}</h1>
+        <div>
+          <button className="btn btn-primary mx-2" onClick={increment}>+</button>
+          <button className="btn btn-primary mx-2" onClick={decrement}>-</button>
+        </div>
+        <br/>
+        <div>
+          <label  className="btn btn-primary mx-2" htmlFor="step">Increment/Decrement</label>
+          <input className="btn btn-primary mx-2"
+            type="number"
+            id="step"
+            defaultValue={step.current}
+            onChange={(e) => setStep(e.target.valueAsNumber)}
+          />
+        </div>
+        <br/>
+        <div>
+          <button className="btn btn-primary mx-2" onClick={reset}>RESET</button>
+        </div>
+      </div>
+    </>
   );
 }
 
